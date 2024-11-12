c9f21a3db094786aed29be1e4a051c0934a05fe5
Gustavo Felipe Morais <gutibrk74@gmail.com>
2024-11-12 10:19:55 -0300

    Delete Lista l directory

--- Lista l/ex2.py
+++
@@ -1,5 +0,0 @@
-a = float(input('Insira a Distância em Quilometros: '))
-
-b = a * 1000
-
-print(f'A distância em Milimetros é: {b}')
--- Lista l/ex3.py
+++
@@ -1,8 +0,0 @@
-d = int(input('Informe os Dias: '))
-h = int(input('Informe as Horas: '))
-m = int(input('Informe os Minutos: '))
-s = int(input('Irforme os Segundos: '))
-
-r = d * 24 * 60 * 60 + h * 60 * 60 + m * 60 + s
-
-print(f'O total em segundos é: {r}')
